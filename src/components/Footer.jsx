1644a2c7cdb92fdd8f44b96d73957183fdddfaf3
Christian Bueno <chmabuen@espol.edu.ec>
2023-02-12 00:07:49 -0500

    add wordpress theme, post as portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='mx-4 md:mx-16 lg:mx-36 xl:mx-64'>
+    <div className='mx-4 mt-4 md:mx-16 lg:mx-36 xl:mx-64 text-center'>
         <div>&copy; 2023 ChristianBueno. All rights reserved. <span>Guayaquil, Ecuador.</span></div>
     </div>
   )
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,20 +1,46 @@
 import React from 'react'
 import {Card} from 'flowbite-react'
+import './Portfolio.css'
 
 const Portfolio = () => {
+  const cards = [
+    {
+      id: 1,
+      imgSrc: './post/wordpress-podman-post.png',
+      title: 'title1 How to create a WordPress containerization environment with podman(a docker alternative) in fedora 36/37 Workstation GNU/Linux ',
+      info: 'Hello to everyone, today we are going to learn how to create a WordPress containerization environment with podman a docker alternative. We need podman, the WordPress image, and a database image MySQL or MariaDB. First we are going to download the images, using the following command.',
+      link: 'https://dev.to/christianbueno1/how-to-create-a-wordpress-containerization-environment-with-podman-in-fedora-36-workstation-gnulinux-2o6k'
+    },
+    {
+      id: 2,
+      imgSrc: './post/kmv-libvirt-windows11.jpg',
+      title: ' Install windows 11 virtual machine on KVM, libvirt, qemu, virt-manager in Linux, fedora 36/37, RPM based distribution ',
+      info: 'The following tutorial will tech you how to create a virtual machine on Linux, first we are going to install the virtual machine tools.',
+      link: 'https://dev.to/christianbueno1/install-windows-11-virtual-machine-on-kvm-libvirt-qemu-virt-manager-in-linux-fedora-3637-rpm-based-distribution-1nkd'
+    },
+  ]
   return (
-    <div className='mx-4 md:mx-16 lg:mx-36 xl:mx-64'>
-      Experience
-      <div className='container'>
-        <div className="max-w-sm">
-          <Card imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg">
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-            </p>
-          </Card>
+    <div name='portfolio' className=' mx-4' >
+      <div className=' max-w-screen-lg mx-auto'>
+        <div className='text-center'>
+          <h2 className='text-4xl font-bold border-b-4 border-cyan-800 inline'>Post & Wordpress Themes</h2>
+
+        </div>
+        <div className='grid_cards md:grid'>
+          {cards.map(({id, imgSrc, title, info, link}) => (
+            <a key={id} target='_blank' href={link}>
+              <div key={id} className=" max-w-sm mx-auto mt-8">
+                <Card key={id} imgSrc={imgSrc}>
+                  <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white line-clamp-3">
+                    {title}
+                  </h5>
+                  <p className="font-normal text-gray-700 dark:text-gray-400 line-clamp-5">
+                    {info}
+                  </p>
+                </Card>
+              </div>
+            </a>
+          ))}
         </div>
       </div>
 
