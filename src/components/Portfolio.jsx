7dff86e93a761c6527afab245b66246f8fa99793
Christian Bueno <chmabuen@espol.edu.ec>
2023-02-13 00:27:26 -0500

    add wordpress theme, news blog blocksy

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -21,17 +21,50 @@ const Portfolio = () => {
     {
       id: 3,
       imgSrc: './post/blocksy-restaurant.png',
-      title: 'Restaurant Theme - Blocksy - Wordpress',
-      info: 'The perfect starter pack for your elegant saloon is here. Meet Restaurant! The site has been created to give you a jump start in designing your next project.',
-      link: 'https://demo.creativethemes.com/blocksy/restaurant/'
+      title: 'Restaurant Theme, Blocksy, Wordpress',
+      info: 'The perfect starter pack for your elegant saloon is here. Meet Restaurant! The site has been created to give you a jump start in designing your next project.', 
+      link: 'https://demo.creativethemes.com/blocksy/restaurant/',
+      price: '$70 Theme only',
     },
     {
       id: 4,
       imgSrc: './post/avalon-anarieldesign.png',
-      title: 'Avalon Theme - ArielDesign - Wordpress',
+      title: 'Avalon Woocommerce Theme, ArielDesign, Wordpress',
       info: 'Avalon is a modern WordPress theme with full site editing capabilities. It is a perfect tool for building professional-looking clothing and fashion stores using WooCommerce.',
       link: 'https://demo.anarieldesign.com/avalon/',
       price: '$80 Theme only',
+    },
+    {
+      id: 5,
+      imgSrc: './post/olorien-woocommerce-wordpress.png',
+      title: 'Olorien Woocommerce Theme, ArielDesign, Wordpress',
+      info: 'Olorien is a modern block-based WooCommerce theme with sophisticated looks ideal for selling cosmetics and beauty products. It is a perfect match for selling cosmetics, beauty salons, and more',
+      link: 'https://demo.anarieldesign.com/olorien/',
+      price: '$60 Theme only',
+    },
+    {
+      id: 6,
+      imgSrc: './post/gadgets-woocommerce-wordpress.png',
+      title: 'Gadgets WooCommerce, Blocksy, Wordpress',
+      info: 'High tech meets high everything. Gadgets is the perfect showcase for your technology e-commerce site. WooCommerce comes pre-installed, so it is super easy to get started. The design is as futuristic as it gets, so setting up your own e-shop and turning heads around is extremely easy.',
+      link: 'https://demo.creativethemes.com/blocksy/gadgets/',
+      price: '$70 Theme only',
+    },
+    {
+      id: 7,
+      imgSrc: './post/tasty-blog-blocksy-wordpress.png',
+      title: 'Tasty Blog Personal, Blocksy, Wordpress',
+      info: 'Yummy! Tasty! Fluffy! These are the emotions that our delicious Tasty starter pack should light up when you first see it. It is a great template to set up a beautiful recipe blog for your peers. The pastel colour scheme and mouth watering images instantly make you try to eat the page you’re on. It’s that good!',
+      link: 'https://demo.creativethemes.com/blocksy/tasty/',
+      price: '$70 Theme only',
+    },
+    {
+      id: 8,
+      imgSrc: './post/news-press-blocksy-wordpress.png',
+      title: 'News Press Blog, Blocksy, Wordpress',
+      info: 'The original, the unbeatable, the cleanest Blog design you might want. Blocksy News is the answer here!.This starter site is super simple and showcases what WordPress has been initially created for — blogging! The homepage is an index of your posts, with beautiful big images and a little excerpt, inviting the visitor to discover more.',
+      link: 'https://demo.creativethemes.com/blocksy/blog/',
+      price: '$70 Theme only',
     },
   ]
   return (
@@ -49,7 +82,7 @@ const Portfolio = () => {
                   <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white line-clamp-3">
                     {title}
                   </h5>
-		  {price && (<small className='absolute bottom-0 right-0 bg-amber-400 p-1 rounded-md'>{price}</small>)}
+                  {price && (<small className='absolute bottom-0 right-0 bg-amber-400 p-1 rounded-md'>{price}</small>)}
                   <p className="font-normal text-gray-700 dark:text-gray-400 line-clamp-5">
                     {info}
                   </p>
